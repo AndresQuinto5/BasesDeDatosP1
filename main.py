ef26d4307525cbe0376bc86524b1c744619ffe42
Andres Emilio Q <aeqv226@gmail.com>
2021-03-23 13:19:46 -0600

    Trabajando Main

--- main.py
+++ main.py
@@ -14,9 +14,87 @@ import ast
 import youtube_dl
 import os
 import random
-Window.clearcolor = (72/255.0, 19/255.0, 95/255.0, 1)
+Window.clearcolor = (222/255.0, 181/255.0, 240/255.0, 1)
 Window.size = (1100, 700)
 
 
 import psycopg2
 import csv
+
+#connect to de database
+con = psycopg2.connect(
+    host = "localhost",
+    database = "proshecto",
+    user = "postgres",
+    password = "admin")
+
+#create a cursor
+cur = con.cursor()
+
+#MongoDB
+import pymongo
+from pymongo import MongoClient
+
+cluster = MongoClient("mongodb+srv://JDiegoSolorzano:Diego199@cluster0-5fvcd.mongodb.net/proyectiFinal?retryWrites=true&w=majority")
+db = cluster["proyectiFinal"]
+collection = db["compras"]
+
+
+class MainWindow(Screen):
+    errM = ObjectProperty(None)
+
+
+    def userVer(self):
+        usern = self.ids.user_field.text
+        passw = self.ids.pass_field.text
+        EleventhWindow.userActual = usern
+        TwelveWindow.userActual = usern
+        ThirteenWindow.userActual = usern
+        FourthWindow.userActual = usern
+        FifteenWindow.userActual = usern
+        FourteenWindow.userActual = usern
+        NineteenWindow.userActual = usern
+
+        if usern != '':
+            cur.execute("SELECT COUNT(*) FROM users WHERE username = %s AND password = %s AND roleid = 3", (str(usern), str(passw)))
+            opcion1 = cur.fetchall()
+            s = str(opcion1)
+            if s != '[(0,)]':
+                self.errM.text = ''
+                self.manager.transition.direction = "left"
+                self.manager.current = 'home'
+            else:
+                self.errM.text = 'Invalid username or password'
+
+
+class SecondWindow(Screen):
+    errM = ObjectProperty(None)
+
+    def userVer(self):
+        usern = self.ids.user_field.text
+        passw = self.ids.pass_field.text
+        EleventhWindow.userActual = usern
+        TwelveWindow.userActual = usern
+        ThirteenWindow.userActual = usern
+        FourthWindow.userActual = usern
+        FifteenWindow.userActual = usern
+        FourteenWindow.userActual = usern
+        NineteenWindow.userActual = usern
+        TwentyWindow.userActual = usern
+
+        if usern != '':
+            cur.execute("SELECT COUNT(*) FROM users WHERE username = %s AND password = %s AND roleid = 1",
+                        (str(usern), str(passw)))
+            opcion1 = cur.fetchall()
+            s = str(opcion1)
+            if s != '[(0,)]':
+
+                self.errM.text = ''
+                self.manager.current = 'homeA'
+                self.manager.transition.direction = "left"
+            else:
+                self.errM.text = 'Invalid username or password'
+
+
+class ThirdWindow(Screen):
+    pass
