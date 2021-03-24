cad7ae42762565dd15178cb1b9ae320eaf0e42ba
Mirka <mirkanicolle@gmail.com>
2021-03-24 11:29:35 -0600

    base de datos

---
+++ proyecto1BD.sql
@@ -0,0 +1,419 @@
+select *
+from track
+
+insert into track(
+	id, name, albumid, genreid, composer, artistid)
+values(
+	1, 'falling', 1, 1, 'glora tino', 1);
+insert into track(
+	id, name, albumid, genreid, composer, artistid)
+values(
+	2, 'trash', 2, 1, 'tina lopez', 1);
+insert into track(
+	id, name, albumid, genreid, composer, artistid)
+values(
+	3, 'hello', 3, 1, 'fernado levy', 1);
+insert into track(
+	id, name, albumid, genreid, composer, artistid)
+values(
+	4, 'table', 4, 1, 'uriel maria', 1);
+insert into track(
+	id, name, albumid, genreid, composer, artistid)
+values(
+	5, 'scream', 5, 1, 'jonah williams', 2);
+insert into track(
+	id, name, albumid, genreid, composer, artistid)
+values(
+	6, 'jump in the house', 6, 2, 'julia tid', 2);
+insert into track(
+	id, name, albumid, genreid, composer, artistid)
+values(
+	7, 'sombrilla', 7, 2, 'nuria chavez', 2);
+insert into track(
+	id, name, albumid, genreid, composer, artistid)
+values(
+	8, 'dance with me', 8, 2, 'miranda vez', 2);
+insert into track(
+	id, name, albumid, genreid, composer, artistid)
+values(
+	9, 'noche de luz', 9, 2, 'aria munch', 2);
+insert into track(
+	id, name, albumid, genreid, composer, artistid)
+values(
+	10, 'en la mañana', 10, 2, 'pamela winter', 2);
+insert into track(
+	id, name, albumid, genreid, composer, artistid)
+values(
+	11, 'en la mañana', 11, 3, 'pamela winter', 2);
+insert into track(
+	id, name, albumid, genreid, composer, artistid)
+values(
+	12, 'mirame', 12, 3, 'yumi nato', 3);
+insert into track(
+	id, name, albumid, genreid, composer, artistid)
+values(
+	13, 'porque', 13, 3, 'lolita parcio', 3);
+insert into track(
+	id, name, albumid, genreid, composer, artistid)
+values(
+	14, 'omega', 14, 3, 'bencio capez', 3);
+insert into track(
+	id, name, albumid, genreid, composer, artistid)
+values(
+	15, 'no mas', 15, 3, 'mojo itla', 4);
+insert into track(
+	id, name, albumid, genreid, composer, artistid)
+values(
+	16, 'papel mojado', 16, 4, 'francis davila', 4);
+insert into track(
+	id, name, albumid, genreid, composer, artistid)
+values(
+	17, 'si no te tengo', 17, 4, 'francisco ruano', 4);
+insert into track(
+	id, name, albumid, genreid, composer, artistid)
+values(
+	18, 'sin palabras', 18, 4, 'frances ima', 5);
+insert into track(
+	id, name, albumid, genreid, composer, artistid)
+values(
+	19, 'nunca', 19, 4, 'lara gullian', 5);
+insert into track(
+	id, name, albumid, genreid, composer, artistid)
+values(
+	20, 'si no fuera por tu amor', 20, 5, 'casie adams', 5);
+insert into track(
+	id, name, albumid, genreid, composer, artistid)
+values(
+	21, 'morir por tu', 1, 6, 'maria casimiro', 5);
+insert into track(
+	id, name, albumid, genreid, composer, artistid)
+values(
+	22, 'pantufla', 2, 6, 'trinidad young', 6);
+insert into track(
+	id, name, albumid, genreid, composer, artistid)
+values(
+	23, 'on top', 3, 6, 'freddie mercury', 6);
+insert into track(
+	id, name, albumid, genreid, composer, artistid)
+values(
+	24, 'fly away', 4, 6, 'pablo palacios', 6);
+insert into track(
+	id, name, albumid, genreid, composer, artistid)
+values(
+	25, 'aire fresco', 5, 6, 'tamara owens', 6);
+
+insert into track(
+	id, name, albumid, genreid, composer, artistid)
+values(
+	26, 'aire fresco', 6, 7, 'tamara owens', 7);
+insert into track(
+	id, name, albumid, genreid, composer, artistid)
+values(
+	27, 'coca cola', 7, 7, 'paloma sus', 7);
+insert into track(
+	id, name, albumid, genreid, composer, artistid)
+values(
+	28, 'trineo', 8, 7, 'felicia viws', 7);
+insert into track(
+	id, name, albumid, genreid, composer, artistid)
+values(
+	29, 'paseito', 9, 7, 'indigo mun', 7);
+insert into track(
+	id, name, albumid, genreid, composer, artistid)
+values(
+	30, 'trabaja', 10, 7, 'lourdes maria', 7);
+	
+insert into track(
+	id, name, albumid, genreid, composer, artistid)
+values(
+	31, 'trabaja', 11, 8, 'lourdes maria', 7);
+insert into track(
+	id, name, albumid, genreid, composer, artistid)
+values(
+	32, 'ventana', 12, 8, 'lourdes maria', 7);
+insert into track(
+	id, name, albumid, genreid, composer, artistid)
+values(
+	33, 'llamada', 13, 8, 'lourdes maria', 7);
+insert into track(
+	id, name, albumid, genreid, composer, artistid)
+values(
+	34, 'over you', 14, 8, 'lourdes maria', 8);
+insert into track(
+	id, name, albumid, genreid, composer, artistid)
+values(
+	35, 'te amo', 15, 8, 'lourdes maria', 8);
+insert into track(
+	id, name, albumid, genreid, composer, artistid)
+values(
+	36, 'i love you', 16, 9, 'lourdes maria', 8);
+insert into track(
+	id, name, albumid, genreid, composer, artistid)
+values(
+	37, 'si a todo', 17, 9, 'lourdes maria', 9);
+insert into track(
+	id, name, albumid, genreid, composer, artistid)
+values(
+	38, 'jamas en la vida', 18, 9, 'lourdes maria', 9);
+insert into track(
+	id, name, albumid, genreid, composer, artistid)
+values(
+	39, 'trabaja', 19, 9, 'lourdes maria', 9);
+insert into track(
+	id, name, albumid, genreid, composer, artistid)
+values(
+	40, 'trabaja', 20, 9, 'lourdes maria', 9);
+	
+	
+insert into track(
+	id, name, albumid, genreid, composer, artistid)
+values(
+	41, 'oracion', 1, 10, 'alex olef', 9);
+insert into track(
+	id, name, albumid, genreid, composer, artistid)
+values(
+	42, 'de la mano', 2, 10, 'nidia linos', 9);
+insert into track(
+	id, name, albumid, genreid, composer, artistid)
+values(
+	43, 'ayudame', 3, 10, 'flor amir', 9);
+insert into track(
+	id, name, albumid, genreid, composer, artistid)
+values(
+	44, 'meneo', 4, 10, 'pilar montenegro', 9);
+insert into track(
+	id, name, albumid, genreid, composer, artistid)
+values(
+	45, 'rastreo virtual', 5, 10, 'santiago roldan', 9);
+insert into track(
+	id, name, albumid, genreid, composer, artistid)
+values(
+	46, 'si fuera ayer', 6, 11, 'felipe mazariegos ', 9);
+insert into track(
+	id, name, albumid, genreid, composer, artistid)
+values(
+	47, 'ahora o nunca', 7, 11, 'elian fonseca', 9);
+insert into track(
+	id, name, albumid, genreid, composer, artistid)
+values(
+	48, 'morir de risa', 8, 11, 'mirian trifa', 10);
+insert into track(
+	id, name, albumid, genreid, composer, artistid)
+values(
+	49, 'claro oscuro', 9, 11, 'walter frut', 10);
+insert into track(
+	id, name, albumid, genreid, composer, artistid)
+values(
+	50, 'colores', 10, 11, 'caddie pun', 10);
+	
+	
+insert into track(
+	id, name, albumid, genreid, composer, artistid)
+values(
+	51, 'rojo', 11, 12, 'alma rueda', 10);
+insert into track(
+	id, name, albumid, genreid, composer, artistid)
+values(
+	52, 'amarillo', 12, 12, 'milo rosales', 10);
+insert into track(
+	id, name, albumid, genreid, composer, artistid)
+values(
+	53, 'azul', 13, 12, 'bear payne', 10);
+insert into track(
+	id, name, albumid, genreid, composer, artistid)
+values(
+	54, 'verde', 14, 12, 'america xion', 10);
+insert into track(
+	id, name, albumid, genreid, composer, artistid)
+values(
+	55, 'rosado', 15, 12, 'susane hill', 10);
+insert into track(
+	id, name, albumid, genreid, composer, artistid)
+values(
+	56, 'morado', 16, 13, 'jared leto', 10);
+insert into track(
+	id, name, albumid, genreid, composer, artistid)
+values(
+	57, 'naranja', 17, 13, 'big sean', 10);
+insert into track(
+	id, name, albumid, genreid, composer, artistid)
+values(
+	58, 'plata', 18, 13, 'jordan qu', 11);
+insert into track(
+	id, name, albumid, genreid, composer, artistid)
+values(
+	59, 'dorado', 19, 13, 'fran jun', 12);
+insert into track(
+	id, name, albumid, genreid, composer, artistid)
+values(
+	60, 'arcoiris', 20, 13, 'perina bub', 12);
+	
+	
+insert into track(
+	id, name, albumid, genreid, composer, artistid)
+values(
+	61, 'estrella', 1, 14, 'perina bub', 12);
+insert into track(
+	id, name, albumid, genreid, composer, artistid)
+values(
+	62, 'luna', 2, 14, 'perina bub', 12);
+insert into track(
+	id, name, albumid, genreid, composer, artistid)
+values(
+	63, 'noche', 3, 14, 'perina bub', 12);
+insert into track(
+	id, name, albumid, genreid, composer, artistid)
+values(
+	64, 'planeta', 4, 14, 'perina bub', 13);
+insert into track(
+	id, name, albumid, genreid, composer, artistid)
+values(
+	65, 'tierra', 5, 14, 'perina bub', 13);
+insert into track(
+	id, name, albumid, genreid, composer, artistid)
+values(
+	66, 'fugaz', 6, 15, 'perina bub', 13);
+insert into track(
+	id, name, albumid, genreid, composer, artistid)
+values(
+	67, 'metioros', 7, 15, 'perina bub', 14);
+insert into track(
+	id, name, albumid, genreid, composer, artistid)
+values(
+	68, 'sol', 8, 15, 'perina bub', 14);
+insert into track(
+	id, name, albumid, genreid, composer, artistid)
+values(
+	69, 'aliens', 9, 15, 'perina bub', 14);
+insert into track(
+	id, name, albumid, genreid, composer, artistid)
+values(
+	70, 'desconocido', 10, 15, 'perina bub', 14);
+
+
+insert into track(
+	id, name, albumid, genreid, composer, artistid)
+values(
+	71, 'paleta', 11, 16, 'yolanda marcos', 15);
+insert into track(
+	id, name, albumid, genreid, composer, artistid)
+values(
+	72, 'cena de dos', 12, 16, 'yolanda marcos', 16);
+insert into track(
+	id, name, albumid, genreid, composer, artistid)
+values(
+	73, 'bonita', 13, 16, 'yolanda marcos', 16);
+insert into track(
+	id, name, albumid, genreid, composer, artistid)
+values(
+	74, 'hermosa', 14, 16, 'yolanda marcos', 16);
+insert into track(
+	id, name, albumid, genreid, composer, artistid)
+values(
+	75, 'lirio', 15, 16, 'yolanda marcos', 16);
+insert into track(
+	id, name, albumid, genreid, composer, artistid)
+values(
+	76, 'rosa', 16, 17, 'yolanda marcos', 16);
+insert into track(
+	id, name, albumid, genreid, composer, artistid)
+values(
+	77, 'girasol', 17, 17, 'yolanda marcos', 17);
+insert into track(
+	id, name, albumid, genreid, composer, artistid)
+values(
+	78, 'leones', 18, 17, 'yolanda marcos', 17);
+insert into track(
+	id, name, albumid, genreid, composer, artistid)
+values(
+	79, 'rapidos', 19, 17, 'yolanda marcos', 17);
+insert into track(
+	id, name, albumid, genreid, composer, artistid)
+values(
+	80, 'fun', 20, 17, 'yolanda marcos', 17);
+	
+	
+insert into track(
+	id, name, albumid, genreid, composer, artistid)
+values(
+	81, 'hermanos del alma', 1, 18, 'jonah williams', 18);
+insert into track(
+	id, name, albumid, genreid, composer, artistid)
+values(
+	82, 'kicks', 2, 18, 'jonah williams', 18);
+insert into track(
+	id, name, albumid, genreid, composer, artistid)
+values(
+	83, 'rows', 3, 18, 'jonah williams', 18);
+insert into track(
+	id, name, albumid, genreid, composer, artistid)
+values(
+	84, 'roar', 4, 18, 'jonah williams', 18);
+insert into track(
+	id, name, albumid, genreid, composer, artistid)
+values(
+	85, 'tell a friend', 5, 18, 'jonah williams', 18);
+insert into track(
+	id, name, albumid, genreid, composer, artistid)
+values(
+	86, 'secrets', 6, 19, 'jonah williams', 18);
+insert into track(
+	id, name, albumid, genreid, composer, artistid)
+values(
+	87, 'filosofy', 7, 19, 'jonah williams', 19);
+insert into track(
+	id, name, albumid, genreid, composer, artistid)
+values(
+	88, 'dimons', 8, 19, 'jonah williams', 19);
+insert into track(
+	id, name, albumid, genreid, composer, artistid)
+values(
+	89, 'angels', 9, 19, 'jonah williams', 19);
+insert into track(
+	id, name, albumid, genreid, composer, artistid)
+values(
+	90, 'looks', 10, 19, 'jonah williams', 19);
+	
+	
+insert into track(
+	id, name, albumid, genreid, composer, artistid)
+values(
+	91, 'lporfin', 11, 20, 'aria munch', 19);
+insert into track(
+	id, name, albumid, genreid, composer, artistid)
+values(
+	92, 'ultimas', 12, 20, 'aria munch', 19);
+insert into track(
+	id, name, albumid, genreid, composer, artistid)
+values(
+	93, 'ya me canse', 13, 20, 'aria munch', 19);
+insert into track(
+	id, name, albumid, genreid, composer, artistid)
+values(
+	94, 'vacaciones', 14, 20, 'aria munch', 19);
+insert into track(
+	id, name, albumid, genreid, composer, artistid)
+values(
+	95, 'me duele', 15, 20, 'aria munch', 19);
+insert into track(
+	id, name, albumid, genreid, composer, artistid)
+values(
+	96, 'agua', 16, 5, 'aria munch', 19);
+insert into track(
+	id, name, albumid, genreid, composer, artistid)
+values(
+	97, 'fresco', 17, 5, 'aria munch', 19);
+insert into track(
+	id, name, albumid, genreid, composer, artistid)
+values(
+	98, 'pelo suelto', 18, 5, 'aria munch', 19);
+insert into track(
+	id, name, albumid, genreid, composer, artistid)
+values(
+	99, 'dormir tranquilo', 19, 5, 'aria munch', 20);
+insert into track(
+	id, name, albumid, genreid, composer, artistid)
+values(
+	100, 'como nadie mas', 20, 6, 'aria munch', 20);
+
+
